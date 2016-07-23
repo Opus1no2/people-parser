a0742e38f4c0dde8264261e1ff079e8aee5bd76f
ttillotson <ttillotson@opinionlab.com>
2016-07-23 14:02:17 -0500

    Adding getDataFromFiles method to get data from all files and adding test. Also spying on console without overriding it

--- scripts/people-parser.js
+++ scripts/people-parser.js
@@ -1,8 +1,10 @@
 'use strict';
+const fs = require('fs');
 
 class PeopleParser {
   constructor(files) {
     this.validateArgs(files);
+    this.files = files;
   }
 
   validateArgs(files) {
@@ -10,6 +12,14 @@ class PeopleParser {
       console.log('Please provide 3 files as input');
       return;
     }
+  }
+
+  getDataFromFiles() {
+   return this.files.map((file) => {
+       return fs.readFileSync(file).toString().trim().split("\n");
+    }).reduce((pre, curr) => {
+      return pre.concat(curr);
+    });
   }
 };
 
--- spec/scripts/people-parser_spec.js
+++ spec/scripts/people-parser_spec.js
@@ -6,7 +6,7 @@ describe('people-parser CLI application', function() {
   it('Should log an error if given less that three files as input', function() {
     const errMsg = 'Please provide 3 files as input';
 
-    console.log = jasmine.createSpy("log");
+    spyOn(console, 'log');
 
     let peopleParser2 = new Parser(['foo', 'bar']);
     expect(console.log).toHaveBeenCalledWith(errMsg);
@@ -16,5 +16,25 @@ describe('people-parser CLI application', function() {
 
     let peopleParser0 = new Parser([]);
     expect(console.log).toHaveBeenCalledWith(errMsg);
+  });
+
+  it('should combine data from all files', function() {
+    const expected = [
+      'White|Lilla|female|magenta|11/8/115',
+      'McGlynn|Teresa|male|maroon|12/4/115',
+      'Bayer|Lionel|female|plum|1/26/116',
+      'Bednar|Blake|male|maroon|1/23/116',
+      'Stokes,Kyle,female,gold,8/2/115',
+      'Flatley,Narciso,male,salmon,4/2/116',
+      'Hills,Pattie,female,azure,5/16/116',
+      'Ledner,Evert,male,purple,9/23/115',
+      'Connell Jude female white 10/19/115',
+      'Tillman Dave male magenta 8/31/115',
+      'Swaniawski Tressie female pink 3/2/116',
+      'Farrell Helene male teal 1/14/116'
+    ];
+
+    const peopleParser = new Parser(['./data/bsv', './data/csv', './data/ssv']);
+    expect(peopleParser.getDataFromFiles()).toEqual(expected);
   });
 });
