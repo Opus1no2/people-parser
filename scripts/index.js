be04da5ad24051490f3f1e2d55bec7ef1c8de230
ttillotson <ttillotson@opinionlab.com>
2016-07-24 13:28:16 -0500

    Adding cli color to index because it looks nice

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 'use strict';
-
-const PeopleParser = require('./people-parser');
+const success = require('cli-color').green
+const PeopleParser = require('../api/server/lib/people-parser');
 
 if (process.argv.slice(2).length < 3) {
   console.log('please provide 3 files as input');
@@ -10,12 +10,12 @@ if (process.argv.slice(2).length < 3) {
 const parser = new PeopleParser(process.argv.slice(2));
 
 console.log('ordered by gender:');
-console.log(parser.ladiesFirst().join('\n'));
+console.log(success(parser.ladiesFirst().join('\n')));
 
 console.log();
 console.log('ordered by birth date:');
-console.log(parser.orderByBirthDate().join('\n'));
+console.log(success(parser.orderByBirthDate().join('\n')));
 
 console.log();
 console.log('ordered by last name:');
-console.log(parser.orderByLastName().join('\n'));
+console.log(success(parser.orderByLastName().join('\n')));
