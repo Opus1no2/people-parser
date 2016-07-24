9a7518a8d7153503bac66a4dce925c3153a5a56d
ttillotson <ttillotson@opinionlab.com>
2016-07-24 16:51:16 -0500

    Updating records index to throw an error for invalid objects. Also adding test to ensure proper responses from records index

--- api/server/routes/index.js
+++ api/server/routes/index.js
@@ -1,10 +1,15 @@
 'use strict';
 
 const PeopleConsumer = require('../lib/people-consumer');
-const file = __dirname + '/../data/csv';
+const file = __dirname + '/../data/post-csv';
 
 module.exports = (req, res) => {
   const consumer = new PeopleConsumer(req.body);
-  consumer.writeToFile(file);
-  res.send(req.body);
+
+  try {
+    consumer.consume(file);
+    res.send('success');
+  } catch (e) {
+    res.send(e.message);
+  }
 }
--- spec/routes/index.spec.js
+++ spec/routes/index.spec.js
@@ -13,9 +13,22 @@ describe("API Server", function() {
       });
     });
 
-    it("returns a response body", function(done) {
+    it("returns an error message if not provided the correct data structure", function(done) {
       request.post(base_url, function(error, response, body) {
-        expect(response.body).toBe("hello from index");
+        expect(response.body).toBe("Invalid object");
+        done();
+      });
+    });
+
+    it("returns success when given a valid data structure", function(done) {
+      const options = {
+        method: 'post',
+        body: {lastname:"Foobar",firstname:"Lionel",gender:"female",color:"plum",birthdate:"1/26/1934"},
+        json: true,
+        url: base_url
+      }
+      request(options, function(error, response, body) {
+        expect(response.body).toBe("success");
         done();
       });
     });
